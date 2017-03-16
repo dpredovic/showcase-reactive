8e5101240ca6d7ec61e3ba280bce62cd441c32d5
predovic <dpredovic@gmail.com>
2017-03-16 10:58:26 +0100

    added id for entity

--- src/main/java/showcase/reactive/controller/TestDTO.java
+++ src/main/java/showcase/reactive/controller/TestDTO.java
@@ -1,10 +1,12 @@
 package showcase.reactive.controller;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class TestDTO {
 
+    @Id
     private final Long id;
     private final String description;
 }
